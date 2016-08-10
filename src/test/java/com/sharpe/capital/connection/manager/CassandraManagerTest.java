f6db8ac57391cce5172038c6fcfdfaf0fd141e89
Lewis Barber <lewisbarber@users.noreply.github.com>
2016-08-10 10:24:47 +0100

    Update CassandraManagerTest.java

--- src/test/java/com/sharpe/capital/connection/manager/CassandraManagerTest.java
+++ src/test/java/com/sharpe/capital/connection/manager/CassandraManagerTest.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.concurrent.Executors;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.datastax.driver.mapping.Mapper;
@@ -17,22 +19,46 @@ public class CassandraManagerTest {
 	private static final String[] HOSTS = { "127.0.0.1" };
 
 	private static final CassandraManager connectionManager = CassandraManager.getInstance(HOSTS, PORT);
+	
+	@BeforeClass
+	public static void setup() {
+		// TODO - Empty fx.tick table
+	}
+	
+	@AfterClass
+	public static void cleanup() {
+		connectionManager.close();
+	}
 
 	@Test
 	public void testAddQuote() throws InterruptedException {
+		
 		Mapper<FxTick> fxTickMapper = connectionManager.getMapper(FxTick.class);
+		
+		// TODO - count records in  fx.tick table
+		
 		for (int i = 0; i < 10; i++) {
+			
 			FxTick quote = new FxTick("AUD/USD", new Date(), BigDecimal.valueOf(1.0D), BigDecimal.valueOf(1.0D));
+			
 			Thread.sleep(5L);
+			
 			Executors.newCachedThreadPool().execute(() -> {
 				fxTickMapper.save(quote);
 			});
+			
 		}
+		
+		// TODO - after Executor completes count records in fx.tick table
+		// TODO - Assert 10 records were added
+		
 	}
 
 	@Test
 	public void testEmptyQuotes() {
+		// TODO - Assert more than 0 records in fx.tick table
 		connectionManager.execute("TRUNCATE fx.tick", FxTick.class);
+		// TODO - Assert 0 records in fx.tick table
 	}
 
 }
